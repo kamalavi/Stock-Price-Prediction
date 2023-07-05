1de84a7584e07ecc32b6a96e7e4af46405d144d4
kamalavi <134662813+kamalavi@users.noreply.github.com>
2023-07-05 11:35:44 -0400

    Data_Extraction_and_Preprocessing

--- stock_prediction.py
+++ stock_prediction.py
@@ -22,10 +22,4 @@ class DataProcessing:
         self.Y_test = self.close[round(train_percent_len*(len(self.close))):len(self.close)]
         self.X_test = self.date[round(train_percent_len*(len(self.date))):len(self.date)]
 
-        # return self.X_train, self.Y_train, self.X_test, self.Y_test
-        return len(self.X_train), len(self.Y_train), len(self.X_test), len(self.Y_test)
-
-aapl = get_stock_data("AAPL","2023-01-01","2023-05-01")
-aapl_preprocessed = DataProcessing(aapl)
-print(aapl_preprocessed.gen_train_test(0.9))
-# print(aapl_preprocessed.close[0:3])
+        return self.X_train, self.Y_train, self.X_test, self.Y_test
